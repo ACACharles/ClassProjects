405117856c1c24931b2e8d27783fab63980dae7b
aapucke <54281045+aapucke@users.noreply.github.com>
2019-08-19 11:12:30 -0700

    Update hello3.java

--- hello3.java
+++ hello3.java
@@ -1,13 +1,24 @@
+//imports scanner
+
 import java.util.Scanner; 
+
+//names class
 class hello3
 {
+	//main methond
 	public static void main(String[] args)
 	
 	{
+		//names scanner input
 		Scanner input = new Scanner(System.in);
+		
+		//prints Hello what is your name
 		System.out.print("Hello! What is your name?: ");
+		
+		//prompts input for name
 		String name = input.nextLine();
 		
+		//prints input for name
 		System.out.println("Hello " + name + ", my name is Drew.");
 		
 	}
