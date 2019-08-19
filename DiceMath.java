d6810fe1fb5bbb44b0f66fca4d90bbc774a06932
ACACharles <54276193+ACACharles@users.noreply.github.com>
2019-08-19 08:51:38 -0500

    comments added no code changes

--- DiceMath.java
+++ DiceMath.java
@@ -1,8 +1,19 @@
+//Importing Random class
 import java.util.Random;
+
+// Create DICE MATH class 
 class DiceMath{
+	
+	//dice num ---- RETURNS int / PARAMETES number of SIDES
 	int diceNum(int sides){
+
+		//CREATE instance of RANDOM
 		Random randomGenerator = new Random();
+
+		// This could have returned the value directly
+		// return = randomGenerator.nextInt(sides) + 1;
 		int randomInt = randomGenerator.nextInt(sides) + 1;
+		
 		//return randomInt;
 		return randomInt;
 	}
