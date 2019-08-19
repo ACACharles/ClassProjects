01a6bd0b64f55239c6ca95789d104f0ef882706f
lmatyja148 <54276185+lmatyja148@users.noreply.github.com>
2019-08-19 11:20:42 -0500

    Update hello4.java

--- hello4.java
+++ hello4.java
@@ -11,7 +11,7 @@ public static void main (String [] args ) {
 	System.out.println( "What is your name?") ;
 	String name = input.next() ; 
 	
-	System.out.println("Hello" + " my name is" + myName) ; 
+	System.out.println("Hello " + name + " my name is" + myName) ; 
 	
 	
 }
