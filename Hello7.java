2a261b78c3127e9273a1396a0edf3ddaaae54300
knford01 <54276167+knford01@users.noreply.github.com>
2019-08-19 13:15:14 -0500

    Update Hello7.java

--- Hello7.java
+++ Hello7.java
@@ -1,13 +1,22 @@
+/* 
+
+This is Kori reviewing file Hello7.java
+App Works
+*/
+
+//import  scanner class
 import java.util.Scanner;
 class Hello7 {
+	//main methoid: starting point for our program
 	public static void main(String[] args) {
 	
+	// Use scanner to obtain input from user and save it as a String variable name
 		Scanner input = new Scanner(System.in);
 	System.out.println(" What is your name: ");
 	
 			String name = input.next();
 			
-			
+	// Print line greeting user			
 	System.out.println( "Hello, " + name + ", welcome to the world. Now leave." + "\n" + " Much love - Hello7.");
 	}
 	
