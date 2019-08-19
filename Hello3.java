8074deedefa64eae386a6de348caa89c5bb6983b
Kdwilson668 <54276047+Kdwilson668@users.noreply.github.com>
2019-08-19 13:22:10 -0500

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,18 @@
+/*
+	This is user 8 commenting on this code.
+*/
+
+import java.util.Scanner; 
+class hello3
+{
+	public static void main(String[] args)
+	
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Hello! What is your name?: ");
+		String name = input.nextLine();
+		
+		System.out.println("Hello " + name + ", my name is Drew.");
+		
+	}
+}
