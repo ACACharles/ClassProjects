bf1636e4e38f88f56e998633255232216898672f
individualtool <54276056+individualtool@users.noreply.github.com>
2019-08-19 13:11:14 -0500

    Update Hello2.java

--- Hello2.java
+++ Hello2.java
@@ -1,3 +1,7 @@
+/*
+This is Marcus reviewing file Hello2
+*/
+
 package help;
 
 import java.util.Scanner;
@@ -5,11 +9,12 @@ import java.util.Scanner;
 public class Hello2 {
 
 	public static void main(String[] args) {
-
+		// looks good
 		System.out.println("Hello User. What is your name?");
 		Scanner name = new Scanner(System.in);
 		String x = name.next();
-
+		// still looking good
 		System.out.println("Hello " + x + ". My name is Kori.");
 	}
 }
+		// fin
