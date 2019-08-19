db1ce2378fd8b99baa34f96c0f4f282d7ea62ce0
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-08-19 11:29:46 -0500

    Update hello1.java

--- hello1.java
+++ hello1.java
@@ -4,11 +4,10 @@ public class hello1 {
 	public static void main(String args[]) {
 		
 		String devname = "1";
-			;
 			
 			Scanner input = new Scanner ( System.in );
 			System.out.println("What is your name?");
-			String username = input.nextLine();
+		String username = input.nextLine();
 			System.out.println("Thank you");
 			System.out.println("Hello " + username + "!");
 			
