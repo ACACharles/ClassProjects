7d77c620bcd39c3ea22d62bd5aca71e8e6c49b38
willwilson1990 <54276192+willwilson1990@users.noreply.github.com>
2019-08-19 11:26:30 -0500

    Add files via upload

---
+++ Hello10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+
+class Hello10
+{
+	public static void main (String [] args)
+	{
+		Scanner name = new Scanner(System.in);
+		
+		String user = name.next();
+		
+		System.out.println ("What is your name?");
+		
+		System.out.println ( "Hello " + user + ", my name is Will.");
+	}
+}
