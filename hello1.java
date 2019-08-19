600417386feec9c3198577fcbbb04d1c47fb038b
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-08-19 11:24:09 -0500

    Add files via upload
    Use this one!

---
+++ hello1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class hello1 {
+	
+	public static void main(String args[]) {
+		
+		String devname = "1";
+			;
+			
+			Scanner input = new Scanner ( System.in );
+			System.out.println("What is your name?");
+			String username = input.nextLine();
+			System.out.println("Thank you");
+			System.out.println("Hello " + username + "!");
+			
+			System.out.println("Hello my name is "+ devname);
+	}
+}
