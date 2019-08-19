aa216e4a0f8d0eaa2eb92aa6bf0ab312852d4e9f
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-08-19 11:35:14 -0500

    Update hello1.java

--- hello1.java
+++ hello1.java
@@ -8,9 +8,9 @@ public class hello1 {
 			Scanner input = new Scanner ( System.in );
 			System.out.println("What is your name?");
 		String username = input.nextLine();
-			System.out.println("Thank you");
+			System.out.println("Thank you!");
 			System.out.println("Hello " + username + "!");
 			
-			System.out.println("Hello my name is "+ devname);
+			System.out.println("Hello my name is "+ devname + "!");
 	}
 }
