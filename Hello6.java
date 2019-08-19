86c14a850257ba9ef3d31b89a70f8856790983db
aapucke <54281045+aapucke@users.noreply.github.com>
2019-08-19 09:06:09 -0700

    Add files via upload

---
+++ Hello6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Hello6
+{
+	public static void main( String [] args)
+	{
+		Scanner input = new Scanner(System.in);
+	System.out.println( "Hello 6");
+	
+	System.out.print ( "What is your name: " );
+		String name = input.next();
+	
+	
+	}
+
+}
