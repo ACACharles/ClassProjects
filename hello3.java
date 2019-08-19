c667347b00870da233373938dc6486352f2939b2
ddunl723 <54276066+ddunl723@users.noreply.github.com>
2019-08-19 10:57:54 -0500

    Add files via upload

---
+++ hello3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; 
+class hello3
+{
+	public static void main(String[] args)
+	
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Hello! What is your name?: ");
+		String name = input.nextLine();
+		
+		System.out.println("Hello " + name + ", my name is Drew.");
+		
+	}
+}
