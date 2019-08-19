f1ddf29666f01bd79dd4c64341a5509676005571
ACACharles <54276193+ACACharles@users.noreply.github.com>
2019-08-19 09:26:30 -0500

    adding DICE pictures

--- DiceRoll.java
+++ DiceRoll.java
@@ -4,6 +4,25 @@ class DiceRoll extends DiceMath{
 		
 		Scanner input = new Scanner ( System.in );
 		
+		String dicePIC = "test";
+
+dicePIC = "              ________\n";
+dicePIC +="             /\\       \\\n";
+dicePIC +="            /  \\       \\\n";
+dicePIC +="           /    \\       \\\n";
+dicePIC +="          /      \\_______\\\n";
+dicePIC +="          \\      /       /\n";
+dicePIC +="        ___\\    /   ____/___\n";
+dicePIC +="       /\\   \\  /   /\\       \\\n";
+dicePIC +="      /  \\   \\/___/  \\       \\\n";
+dicePIC +="     /    \\       \\   \\       \\\n";
+dicePIC +="    /      \\_______\\   \\_______\\\n";
+dicePIC +="    \\      /       /   /       /\n";
+dicePIC +="     \\    /       /   /       /\n";
+dicePIC +="      \\  /       /\\  /       /\n";
+dicePIC +="       \\/_______/  \\/_______/    \n";
+	   
+		
 		System.out.print( "Enter number of sides: ");
 		int sides = input.nextInt();
 		
@@ -12,11 +31,17 @@ class DiceRoll extends DiceMath{
 		System.out.print ( "Enter number of dice to roll: " );
 		int numberOfDice = input.nextInt();
 		
+		System.out.println(dicePIC);
+		
 		for ( int i = 0; i < numberOfDice; i++){
 			
 			int num = dice.diceNum(sides);
 			
 			System.out.println("Dice number " +  (i + 1) + " is: " + num);
 		}
+		
+		System.out.println(dicePIC);
 	}
+	
+	
 }
