533d7c4c8d9d884016b89fafba6e4fe4006cee65
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-08-19 13:18:41 -0500

    Add files via upload

--- Hello6.java
+++ Hello6.java
@@ -1,12 +1,21 @@
+/*
+
+This is Zackery review file Hello6
+
+*/
+
+//Importing scanner for user input
 import java.util.Scanner;
 
 class Hello6
 {
 	public static void main( String [] args)
 	{
+	//Greeting
 		Scanner input = new Scanner(System.in);
 	System.out.println( "Hey guy");
 	
+	//Asks user and promts them to write their name
 	System.out.print ( "What is your name: " );
 		String name = input.next();
 		
