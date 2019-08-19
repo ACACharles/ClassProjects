86884da04a5c98cc4bfe11a317b1156f8a12300b
ddunl723 <54276066+ddunl723@users.noreply.github.com>
2019-08-19 13:13:54 -0500

    Add files via upload
    Added some comments

--- Hello8.java
+++ Hello8.java
@@ -1,12 +1,13 @@
+//This is Drew reviewing file Hello8.java
 import java.util.Scanner;
 public class Hello8{
 	public static void main (String[] args){
 		Scanner input = new Scanner(System.in);
-		
+		//Could make println into print so that the input is on the same line as the question
 		System.out.println("What is your name?");
-		
+		//Sets userName as the user input
 		String userName = input.nextLine();
-		
+		//Prints out the user input as well as the included strings
 		System.out.println("Hello " + userName + "." + " My name is Keith.");
 	}
 }
