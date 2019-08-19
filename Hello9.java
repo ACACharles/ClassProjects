71db823134866a4dbf209729a92ef923e926ffa7
Brian3207 <54276129+Brian3207@users.noreply.github.com>
2019-08-19 11:22:56 -0500

    Add files via upload
    This is the first one you suggested

---
+++ Hello9.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Hello9 {
+
+	public static void main ( String[] args ){
+		
+		Scanner input = new Scanner ( System.in );
+		System.out.print("What is your name? : ");
+		
+		String user = input.nextLine() ;
+		
+		System.out.println( "Hello " + user + "My name is 9." );
+		
+	}
+	
+	
+}
