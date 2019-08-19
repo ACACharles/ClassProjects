13f929cbf7677257d85d490d41196bbdae060205
ACACharles <54276193+ACACharles@users.noreply.github.com>
2019-08-19 13:38:08 -0500

    Update hello1.java
    I added comments and stuff

--- hello1.java
+++ hello1.java
@@ -1,16 +1,28 @@
+/*
+
+This is CHARLES reviewing file hello1.java
+DOSE IT WORK (YES/ NO) it better work
+*/
 import java.util.Scanner;
 public class hello1 {
 	
 	public static void main(String args[]) {
 		
+//creates a string named "DEVNAME"
 		String devname = "1";
-			
+//Creates a new instance of Scanner call 'input' that is grabing info from "system.in"			
 			Scanner input = new Scanner ( System.in );
+//Promts the user for their name
 			System.out.println("What is your name?");
+//Creates an String to save username.... 
+//not grouped with the rest of variables created
 		String username = input.nextLine();
+
+//thanks the user
 			System.out.println("Thank you!");
+//talks to user
 			System.out.println("Hello " + username + "!");
-			
+//introduces self to user			
 			System.out.println("Hello my name is "+ devname + "!");
 	}
 }
