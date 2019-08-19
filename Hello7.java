e59d86a574193d2aab3b2fc918259547c78ed944
individualtool <54276056+individualtool@users.noreply.github.com>
2019-08-19 11:16:36 -0500

    Add files via upload

---
+++ Hello7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Hello7 {
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+	System.out.println(" What is your name: ");
+	
+			String name = input.next();
+			
+			
+	System.out.println( "Hello, " + name + ", welcome to the world. Now leave.");
+	
+
+	}
+
+	}
