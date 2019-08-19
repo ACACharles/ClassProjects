79bbe232ae5934b81d8a3d3e78e34a98ef288dc9
lmatyja148 <54276185+lmatyja148@users.noreply.github.com>
2019-08-19 11:16:32 -0500

    Add files via upload

---
+++ hello4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class four
+{
+public static void main (String [] args ) {
+	
+	String myName = "Luke";
+	Scanner input = new Scanner ( System.in );
+	
+
+	
+	System.out.println( "What is your name?") ;
+	String name = input.next() ; 
+	
+	System.out.println("Hello" + " my name is" + myName) ; 
+	
+	
+}
+}
