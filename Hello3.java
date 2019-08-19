823268456f4b1042e8b0390233fe42e3f4f846c0
Kdwilson668 <54276047+Kdwilson668@users.noreply.github.com>
2019-08-19 13:28:34 -0500

    Add files via upload

--- Hello3.java
+++ Hello3.java
@@ -1,17 +1,20 @@
 /*
 	This is user 8 commenting on this code.
 */
-
+// Imports the scanner class
 import java.util.Scanner; 
 class hello3
 {
 	public static void main(String[] args)
 	
 	{
+		//Creates a new object named "input" from the Scanner class and calls upon the System.in method from that class.
 		Scanner input = new Scanner(System.in);
+		//Prompts the user for the desired input.
 		System.out.print("Hello! What is your name?: ");
+		//Accepts input from the user in the user in the form of a String that has been named "name".
 		String name = input.nextLine();
-		
+		//Prints out to screen string literals concatonated with the user input.
 		System.out.println("Hello " + name + ", my name is Drew.");
 		
 	}
