01dc5d48cfe2fe3ee6b47673e41852e9c625c6cd
Marcus <54276056+individualtool@users.noreply.github.com>
2019-09-05 11:54:15 -0500

    Add files via upload

---
+++ errorCatchMarcus.java
@@ -0,0 +1,21 @@
+package CB;
+
+public class errorCatchMarcus {
+	public static void main(String args[]) {
+int nums1[] = {4, 6, 7, 9, 32, 11};
+int nums2[] = {3, 0, 5, 0};
+String a = "hams";
+int b = 6;
+
+	for (int i=0; i<nums1.length; i++) {
+		System.out.println(nums1[i] + " / " + nums2[i] + " is " + nums1[i]/nums2[i]);
+	}
+	
+	System.out.println(b = a);
+
+	}
+
+	
+	}
+
+
