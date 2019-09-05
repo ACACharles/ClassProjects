264f8b84a5d98937937218b16a9ce83629f31359
lmatyja148 <54276185+lmatyja148@users.noreply.github.com>
2019-09-05 11:49:31 -0500

    Add files via upload

---
+++ AssignCatchLUKE.java
@@ -0,0 +1,34 @@
+package random;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AssignCatchLUKE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 5 ; 
+		int k = 0; 
+		int data ; 
+		int [] myArray = {7, 4, 3, 2,}; 
+		int l = 0 ;
+		String str  = String.ValueOf(l) ;
+		
+			data = j/k ; 
+			
+		
+		
+		
+		System.out.println(data); 
+		System.out.println("Elements in the array are: ");
+		 System.out.println(Arrays.toString(myArray));
+		Scanner sc = new Scanner(System.in) ;
+		System.out.println("Enter the index of element :: ");
+		int element = sc.nextInt();
+		System.out.println(" Element in the given index is : " + myArray[element]);
+		System.out.println(l);
+
+		}
+	}
+
+
