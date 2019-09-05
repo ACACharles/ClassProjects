6ac1a21919542e0ffd26dd37ae94fc40ecb2086d
aapucke <54281045+aapucke@users.noreply.github.com>
2019-09-05 12:21:12 -0700

    Add files via upload

--- fun.java
+++ fun.java
@@ -1,22 +1,49 @@
 class fun
-{
+{	
+	
+
 	public static void main(String [] args)
 	{
+			try{
 		int nums [] = new int [7];
 		
 		String noFun = new String();
 		
 		nums [9] = 12;
 		
-		noFun = 2345;
+		//nums[5] = "andrew";
 		
-		/*try
-		{
+		//noFun = 2345;
+		
+		int nums1 = 100 / 0;
+		
+			}
 			catch(ArrayIndexOutOfBoundsException aiob)
 			{
 				System.out.println(" Index is out of bounds. ");
 			} 
-		} */
+			catch(ArithmeticException ae)
+			{
+			System.out.println("Cannot divide by zero.");	
+			}
+			catch(NumberFormatException nfe)
+			{
+				System.out.println("Not a valid input.");
+			}
+			/*catch(Exception name)
+			{
+				System.out.println(name); 
+			}
+*/			catch(IllegalArgumentException iae)
+			{
+				System.out.println("Wrong.");
+			} 
+			finally
+			{
+				System.out.println("Most errors have been caught");
+			}
+			
+		 
 		
 	}
 }
