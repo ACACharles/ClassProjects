2b2f80f103138b4dc3c2aa522e02c677bb6effe7
aapucke <54281045+aapucke@users.noreply.github.com>
2019-09-05 09:40:32 -0700

    Add files via upload

---
+++ fun.java
@@ -0,0 +1,22 @@
+class fun
+{
+	public static void main(String [] args)
+	{
+		int nums [] = new int [7];
+		
+		String noFun = new String();
+		
+		nums [9] = 12;
+		
+		noFun = 2345;
+		
+		/*try
+		{
+			catch(ArrayIndexOutOfBoundsException aiob)
+			{
+				System.out.println(" Index is out of bounds. ");
+			} 
+		} */
+		
+	}
+}
