c6cfcf772dd2d4295d3d08932004db17974f788f
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-09-05 12:28:34 -0500

    Add files via upload

---
+++ tryzackery.java
@@ -0,0 +1,15 @@
+
+public class tryzackery {
+	
+	public static void main(String args[]) {
+		
+			int num1[] = {2, 4, 9, 3, 6, 10};
+			int num2[] = {1, 5, 7, 8};
+			String number = "This is my string, there are many like it. ";
+			
+			
+				try { 
+					System.out.println(num1[i] + " / " + num2[i] + " is " + num1[i]/num2[i]); 
+
+				}
+}
