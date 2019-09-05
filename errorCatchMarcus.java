9ce1350bec39ed33d969f8257bd2d7b54808411d
Marcus <54276056+individualtool@users.noreply.github.com>
2019-09-05 15:44:16 -0500

    Add files via upload

---
+++ errorCatchMarcus.java
@@ -0,0 +1,44 @@
+package CB;
+
+
+public class errorCatchMarcus {
+	public static void main(String args[]) {
+int nums1[] = {4, 6, 7, 9, 32, 11};
+int nums2[] = {3, 0, 5, 0};
+
+
+
+
+	for (int i=0; i<nums1.length; i++) {
+		try {
+		System.out.println(nums1[i] + " / " + nums2[i] + " is " + nums1[i]/nums2[i]);
+		
+		System.out.println();
+		}
+	
+		
+	catch(ArrayIndexOutOfBoundsException exc) {
+		System.out.println("You gotta be quicker than that");
+		System.out.println();
+		
+	} 
+		
+	
+	catch(ArithmeticException exc) {
+		System.out.println("Can't divide by zero");
+		System.out.println();
+	}
+	finally {
+		System.out.println("i like them french fried taters..");
+		System.out.println();
+	}
+
+	}
+}
+
+}
+	
+	
+	
+	
+
