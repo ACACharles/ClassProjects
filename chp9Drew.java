fc2de2cc2316891b03b0ff8c7493a17471fae689
ddunl723 <54276066+ddunl723@users.noreply.github.com>
2019-09-05 11:35:30 -0500

    Add files via upload

---
+++ chp9Drew.java
@@ -0,0 +1,18 @@
+package bleh;
+
+public class chp9Drew {
+	public static void main(String args[]) {
+		int[] x= {2,0};
+		
+		int div=0;
+		int bound=0;
+		int notstr;
+		String str="This is a string";
+		div=x[0]/x[1];
+		bound=x[0]/x[2];
+		
+		notstr=str;		
+		
+		System.out.print(div+bound+notstr);
+	}
+}
