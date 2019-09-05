6e7beb0e87310507634f633d6845643274a06e7f
Kdwilson668 <54276047+Kdwilson668@users.noreply.github.com>
2019-09-05 12:35:16 -0500

    Add files via upload

--- KWilsonErrors.java
+++ KWilsonErrors.java
@@ -1,11 +1,24 @@
-public class KWilsonErrors {
+import java.util.InputMismatchException;
+
+public class KWilsonErrors{
 
 	public static void main(String[] args) {
 		int a = 0;
 		int b = 0;
 		int c = 0;
-		String str = 0;
-		
+		String str = "Some string";
+		int[] arr = new int[5];
+		String x = "";
+		try {
+		str += Integer.parseInt(str);
+		System.out.println(arr[8]);
 		c = a / b;
+		}
+		catch (ArithmeticException|ArrayIndexOutOfBoundsException|NumberFormatException ex) {
+			x = ex.toString();
+		}
+		finally {
+			System.out.println("All errors have been caught. Error was: " + x);
+		}
 	}
 }
