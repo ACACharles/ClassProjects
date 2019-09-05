d2cb9a44f111dc00f7ef9368dffc5efa614b9cd8
willwilson1990 <54276192+willwilson1990@users.noreply.github.com>
2019-09-05 12:24:05 -0500

    Add files via upload

---
+++ exceptionWorkWILL.java
@@ -0,0 +1,10 @@
+class exceptionWork{
+	int [] row1 = {2, 6, 3, 17, 9};
+	int [] row2 = {0, 3, 6};
+	
+	public static void main(String [] args){
+		System.out.println( row1[1] / row2[0]);
+		row2[3] = 8;
+		String convert = row1[3];
+	}
+}
