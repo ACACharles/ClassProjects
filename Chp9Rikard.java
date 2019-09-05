76890e99105f34fafdf6fc0e90c2065dfa20bf23
Ozark1989 <54276118+Ozark1989@users.noreply.github.com>
2019-09-05 11:55:45 -0500

    Add files via upload

---
+++ Chp9Rikard.java
@@ -0,0 +1,17 @@
+package MyWork;
+
+public class Chp9Rikard {
+
+	public static void main(String[] args) {
+		int numer[] = {4, 8, 16, 32, 64, 128, 256};
+		int denom[] = {2, 0, 4, 4, 0, 8};
+
+		System.out.println(numer[i] + "/" + denom[i] + " is " numer[i] / denom[i]);
+
+		int str1 = 0;
+		String srt2 = str1;
+
+		System.out.println(str1);
+		
+	}
+}
