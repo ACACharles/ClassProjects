9baf8b0165ee4c8a6702b907b17cd86893efc32d
knford01 <54276167+knford01@users.noreply.github.com>
2019-10-02 15:31:04 -0500

    Update KoriSalad.sql

--- SQL/KoriSalad.sql
+++ SQL/KoriSalad.sql
@@ -1,12 +1,5 @@
-SELECT * from salad
-SELECT * from saladIngredient
-SELECT * from Fruit
-SELECT * from Other
-
-select name.
-
-
-
+insert into salad
+values (200, 107, 1, 3, 1, 2, 1, 4, 1, 11, 1, 317)
 
 
 
