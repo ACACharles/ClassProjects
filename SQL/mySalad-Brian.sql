19de5e4940a06be0c5882c59a539c1c5b010eefc
Brian3207 <54276129+Brian3207@users.noreply.github.com>
2019-10-02 15:25:28 -0500

    Add files via upload

---
+++ SQL/mySalad-Brian.sql
@@ -0,0 +1,43 @@
+select*  from saladIngredient
+select * from Fruit
+select * from plant
+select * from Veggy
+select * from salad
+select * from Other
+
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values (801, 103, 1 ,3)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values(802, 103, 1, 5)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values(803, 103, 1, 1 )
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(804, 103, 1, 302)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(805, 103, 1, 307)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(806, 103, 1, 308)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(807, 103, 1, 309)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(808, 103, 1, 310)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(809, 103, 1, 306)
+
+
+
+
+
+
+
+
+
+
