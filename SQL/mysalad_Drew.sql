4e82697886eaf4cefb027c6eb2200b5bea9e1782
ddunl723 <54276066+ddunl723@users.noreply.github.com>
2019-10-02 15:10:24 -0500

    Add files via upload

---
+++ SQL/mysalad_Drew.sql
@@ -0,0 +1,20 @@
+insert into saladIngredient(id, Sid, isVeggy, VeggyID)
+values(300, 106, 1, 6) 
+
+insert into saladIngredient(id, Sid,isNut ,NutID )
+values(301, 106,1 ,25 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(302, 106,1 ,7 ) 
+
+insert into saladIngredient(id, Sid,isFruit ,FruitID )
+values(303, 106,1 ,11 ) 
+
+insert into saladIngredient(id, Sid,isPlant ,PlantID )
+values(304, 106,1 ,7 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(305, 106,1 ,4 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(306, 106,1 ,5 ) 
