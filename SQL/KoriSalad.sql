f54adca996d2d985892732c0239cba1c5cabe458
knford01 <54276167+knford01@users.noreply.github.com>
2019-10-02 15:50:34 -0500

    Update KoriSalad.sql

--- SQL/KoriSalad.sql
+++ SQL/KoriSalad.sql
@@ -1,6 +1,11 @@
-insert into salad
-values (200, 107, 1, 3, 1, 2, 1, 4, 1, 11, 1, 317)
-
-
-
-
+INSERT INTO saladIngredient (Sid, id, isFruit, FruitID)
+	VALUES(107, 201, 1, 2)
+INSERT INTO saladIngredient (Sid, id, isPlant, PlantID)
+	VALUES(107, 202, 1, 5)
+			(107, 203, 1, 3)
+INSERT INTO saladIngredient (Sid, id, isVeggy, VeggyID)
+	VALUES(107, 204, 1, 4)
+INSERT INTO saladIngredient (Sid, id, isNut, NutID)
+	VALUES(107, 205, 1, 11)
+INSERT INTO saladIngredient (Sid, id, isOther, OtherID)
+	VALUES(107, 206, 1, 312)
