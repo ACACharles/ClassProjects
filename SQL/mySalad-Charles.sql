a8aa18c92d5048dd0b18bdab5e23157b7dfdb4eb
ACACharles <54276193+ACACharles@users.noreply.github.com>
2019-10-02 14:48:08 -0500

    Create mySalad-Charles.sql
    My salad

---
+++ SQL/mySalad-Charles.sql
@@ -0,0 +1,28 @@
+select * from  saladIngredient
+
+--insert into saladIngredient (id, Sid, isPlant , PlantID )
+--values (7, 111,1, 4)
+
+--insert into saladIngredient (id, Sid, isPlant , PlantID )
+--values (2, 111,1, 6)
+
+--insert into saladIngredient (id, Sid, isNut  , NutID )
+--values (3, 111, 1, 25)
+
+--insert into saladIngredient (id, Sid, isNut  , NutID )
+--values (4, 111, 1, 4)
+
+--insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
+--values (5, 111, 1, 2)
+
+--insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
+--values (6, 111, 1, 10)
+
+--insert into saladIngredient (id, Sid, isFruit  , FruitID )
+--values (1, 111, 1, 11)
+
+--insert into saladIngredient (id, Sid, isFruit  , FruitID )
+--values (8, 111, 1, 10)
+
+--insert into saladIngredient (id, Sid, isOther , OtherID )
+--values (9, 111, 1, 307)
