9ad5e03efe2c50a693a235fe6a8dea8b563ecfce
ZackerycFox <54276062+ZackerycFox@users.noreply.github.com>
2019-10-02 15:23:03 -0500

    Add files via upload

---
+++ SQL/MySQL-Zackery100.sql
@@ -0,0 +1,37 @@
+select * from saladIngredient
+
+insert into saladIngredient (id, Sid, isplant, plantID)
+values (100, 108, 1, 4)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (102, 108, 1, 5)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (103, 108, 1, 5)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (104, 108, 1, 4)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (105, 108, 1, 17)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values (106, 108, 1, 1)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values (107, 108, 1, 3)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (108, 108, 1, 315)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (109, 108, 1, 319)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (110, 108, 1, 318)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (111, 108, 1, 315)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (112, 108, 1, 313)
