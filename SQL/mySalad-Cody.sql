9fe388a908618238a23a62c2ad4636717db7e2e2
Ozark1989 <54276118+Ozark1989@users.noreply.github.com>
2019-10-02 15:12:36 -0500

    Add files via upload

---
+++ SQL/mySalad-Cody.sql
@@ -0,0 +1,13 @@
+/* Cucumber Salad */
+Select * from garden
+
+INSERT INTO saladIngredient (id, Sid, isVeggy, VeggyID)
+	VALUES (501, 105,1, 5 )
+INSERT INTO saladIngredient (id, Sid, isVeggy, VeggyID)
+	VALUES (502, 105, 1, 4)
+INSERT INTO saladIngredient (id, Sid, isFruit, FruitID)
+	VALUES (503, 105, 1, 1)
+INSERT INTO saladIngredient (id, Sid, isOther, OtherID)
+	VALUES (504, 105, 1, 313)
+
+
