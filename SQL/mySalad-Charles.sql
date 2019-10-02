4ccbe23089a2858570572dcd515ee7339c20ab9e
ACACharles <54276193+ACACharles@users.noreply.github.com>
2019-10-02 16:07:40 -0500

    Update mySalad-Charles.sql
    master script to insert salad

--- SQL/mySalad-Charles.sql
+++ SQL/mySalad-Charles.sql
@@ -1,28 +1,224 @@
-select * from  saladIngredient
+select * from saladIngredient 
+--Charles
+insert into saladIngredient (id, Sid, isPlant , PlantID )
+values (7, 111,1, 4)
 
---insert into saladIngredient (id, Sid, isPlant , PlantID )
---values (7, 111,1, 4)
+insert into saladIngredient (id, Sid, isPlant , PlantID )
+values (2, 111,1, 6)
 
---insert into saladIngredient (id, Sid, isPlant , PlantID )
---values (2, 111,1, 6)
+insert into saladIngredient (id, Sid, isNut  , NutID )
+values (3, 111, 1, 25)
 
---insert into saladIngredient (id, Sid, isNut  , NutID )
---values (3, 111, 1, 25)
+insert into saladIngredient (id, Sid, isNut  , NutID )
+values (4, 111, 1, 4)
 
---insert into saladIngredient (id, Sid, isNut  , NutID )
---values (4, 111, 1, 4)
+insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
+values (5, 111, 1, 2)
 
---insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
---values (5, 111, 1, 2)
+insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
+values (6, 111, 1, 10)
 
---insert into saladIngredient (id, Sid, isVeggy  , VeggyID )
---values (6, 111, 1, 10)
+insert into saladIngredient (id, Sid, isFruit  , FruitID )
+values (1, 111, 1, 11)
 
---insert into saladIngredient (id, Sid, isFruit  , FruitID )
---values (1, 111, 1, 11)
+insert into saladIngredient (id, Sid, isFruit  , FruitID )
+values (8, 111, 1, 10)
 
---insert into saladIngredient (id, Sid, isFruit  , FruitID )
---values (8, 111, 1, 10)
+insert into saladIngredient (id, Sid, isOther , OtherID )
+values (9, 111, 1, 307)
 
---insert into saladIngredient (id, Sid, isOther , OtherID )
---values (9, 111, 1, 307)
+--KEITH
+INSERT INTO saladIngredient (Sid, id, isFruit, FruitID)
+	VALUES(104, 701, 1, 6),
+		  (104, 702, 1, 2),
+		  (104, 703, 1, 7),
+		  (104, 704, 1, 8),
+		  (104, 705, 1, 9)
+INSERT INTO saladIngredient (Sid, id, isOther, OtherID)
+	VALUES(104, 706, 1, 311),
+		  (104, 707, 1, 312)
+
+--Marcus
+insert into saladingredient(id, Sid, isPlant, PlantID) 
+values(1001, 102, 1, 3)
+
+insert into saladingredient(id, Sid, isother, otherid) 
+values(1002, 102, 1, 301)
+
+insert into saladingredient(id, Sid, isFruit, fruitid) 
+values(1003, 102, 1, 11)
+
+insert into saladingredient(id, Sid, isFruit, Fruitid) 
+values(1004, 102, 1, 1)
+
+insert into saladingredient(id, Sid, isOther, otherid) 
+values(1005, 102, 1, 304)
+
+insert into saladingredient(id, Sid, isveggy, veggyid) 
+values(1006, 102, 1, 4)
+
+--DREW
+
+insert into saladIngredient(id, Sid, isVeggy, VeggyID)
+values(300, 106, 1, 6) 
+
+insert into saladIngredient(id, Sid,isNut ,NutID )
+values(301, 106,1 ,25 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(302, 106,1 ,7 ) 
+
+insert into saladIngredient(id, Sid,isFruit ,FruitID )
+values(303, 106,1 ,11 ) 
+
+insert into saladIngredient(id, Sid,isPlant ,PlantID )
+values(304, 106,1 ,7 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(305, 106,1 ,4 ) 
+
+insert into saladIngredient(id, Sid,isVeggy ,VeggyID )
+values(306, 106,1 ,5 ) 
+
+--Cody
+INSERT INTO saladIngredient (id, Sid, isVeggy, VeggyID)
+	VALUES (501, 105,1, 5 )
+INSERT INTO saladIngredient (id, Sid, isVeggy, VeggyID)
+	VALUES (502, 105, 1, 4)
+INSERT INTO saladIngredient (id, Sid, isFruit, FruitID)
+	VALUES (503, 105, 1, 1)
+INSERT INTO saladIngredient (id, Sid, isOther, OtherID)
+	VALUES (504, 105, 1, 313)
+
+-- Zak
+
+insert into saladIngredient (id, Sid, isplant, plantID)
+values (100, 108, 1, 4)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (102, 108, 1, 5)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (103, 108, 1, 5)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (104, 108, 1, 4)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values (105, 108, 1, 17)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values (106, 108, 1, 1)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values (107, 108, 1, 3)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (108, 108, 1, 315)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (109, 108, 1, 319)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (110, 108, 1, 318)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (111, 108, 1, 315)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (112, 108, 1, 313)
+
+--  Andrew
+
+insert into saladIngredient(id, sid, isVeggy, VeggyID)
+values(901 , 109, 1,2)
+
+insert into saladIngredient(id, sid, isNut, NutID)
+values(902, 109, 1, 17)
+insert into saladIngredient(id, sid, isFruit, FruitID)
+values(903, 109, 1, 12)
+
+insert into saladIngredient(id, sid, isFruit, FruitID)
+values(904, 109, 1, 2)
+
+insert into saladIngredient(id, sid, isOther, OtherID)
+values(905, 109,1,304)
+
+--  will
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values ( 600, 101, 1, 301)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (601, 101, 1, 302)
+
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values (602, 101, 1, 4)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (603, 101, 1, 303)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (604, 101, 1, 308)
+
+-- Brian
+
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values (801, 103, 1 ,3)
+
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values(802, 103, 1, 5)
+
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values(803, 103, 1, 1 )
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(804, 103, 1, 302)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(805, 103, 1, 307)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(806, 103, 1, 308)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(807, 103, 1, 309)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(808, 103, 1, 310)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(809, 103, 1, 306)
+
+-- LUKE
+
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values(400,110,1,5)
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values(401,110,1,4)
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+values(402,110,1,1)
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values(403,110,1,5)
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(404,110,1,302) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(405,110,1,304) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(406,110,1,308) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(407,110,1,317) 
+
+-- Kori
+
+INSERT INTO saladIngredient (Sid, id, isFruit, FruitID)
+	VALUES(107, 201, 1, 2)
+INSERT INTO saladIngredient (Sid, id, isPlant, PlantID)
+	VALUES(107, 202, 1, 5),
+			(107, 203, 1, 3)
+INSERT INTO saladIngredient (Sid, id, isVeggy, VeggyID)
+	VALUES(107, 204, 1, 4)
+INSERT INTO saladIngredient (Sid, id, isNut, NutID)
+	VALUES(107, 205, 1, 11)
+INSERT INTO saladIngredient (Sid, id, isOther, OtherID)
+	VALUES(107, 206, 1, 312)
