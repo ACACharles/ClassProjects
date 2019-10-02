31ce0c8924310060e53888f9c2bfdb8bb061bc5d
lmatyja148 <54276185+lmatyja148@users.noreply.github.com>
2019-10-02 15:35:23 -0500

    Add files via upload

---
+++ SQL/MySaLaD-LUKE.sql
@@ -0,0 +1,24 @@
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values(400,110,1,5)
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values(401,110,1,4)
+insert into saladIngredient (id, Sid, isFruit, FruitID)
+value(402,110,1,1)
+insert into saladIngredient (id, Sid, isVeggy, VeggyID)
+values(403,110,1,5)
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(404,110,1,302) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(405,110,1,304) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(406,110,1,308) 
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values(407,110,1,317) 
+
+
+
+SELECT * from plant
+SELECT * from Veggy
+SELECT * from Other
+SELECT * from Fruit
+SELECT * from salad
