16d3d764b0166dd5ae68ad7d03f8b80fe5c51397
willwilson1990 <54276192+willwilson1990@users.noreply.github.com>
2019-10-02 15:25:27 -0500

    Add files via upload

---
+++ SQL/mySalad-Will.sql
@@ -0,0 +1,16 @@
+select * from saladIngredient
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values ( 600, 101, 1, 301)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (601, 101, 1, 302)
+
+insert into saladIngredient (id, Sid, isPlant, PlantID)
+values (602, 101, 1, 4)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (603, 101, 1, 303)
+
+insert into saladIngredient (id, Sid, isOther, OtherID)
+values (604, 101, 1, 308)
