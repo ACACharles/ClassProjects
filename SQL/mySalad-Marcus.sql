b7b23b95b7e5ee6d37a2ddfc60458038b90819ba
Marcus <54276056+individualtool@users.noreply.github.com>
2019-10-02 15:05:14 -0500

    Add files via upload

---
+++ SQL/mySalad-Marcus.sql
@@ -0,0 +1,18 @@
+
+insert into saladingredient(id, Sid, isPlant, PlantID) 
+values(1001, 102, 1, 3)
+
+insert into saladingredient(id, Sid, isother, otherid) 
+values(1002, 102, 1, 301)
+
+insert into saladingredient(id, Sid, isFruit, fruitid) 
+values(1003, 102, 1, 11)
+
+insert into saladingredient(id, Sid, isFruit, Fruitid) 
+values(1004, 102, 1, 1)
+
+insert into saladingredient(id, Sid, isOther, otherid) 
+values(1005, 102, 1, 304)
+
+insert into saladingredient(id, Sid, isveggy, veggyid) 
+values(1006, 102, 1, 4)
